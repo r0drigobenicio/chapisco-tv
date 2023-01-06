a17c65a21339df9d926fc6f687bd7ab990c33b21
Rodrigo Benício <rodrigobeniciosilva@gmail.com>
2023-01-06 14:38:26 -0300

    add login_controller

---
+++ lib/controllers/login/login_controller.dart
@@ -0,0 +1,18 @@
+import 'package:mobx/mobx.dart';
+part 'login_controller.g.dart';
+
+class LoginController = _LoginControllerBase with _$LoginController;
+
+abstract class _LoginControllerBase with Store {
+  @observable
+  String email = '';
+
+  @action
+  void setEmail(String value) => email = value;
+
+  @observable
+  String password = '';
+
+  @action
+  void setPassword(String value) => password = value;
+}
---
+++ lib/controllers/login/login_controller.g.dart
@@ -0,0 +1,76 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_controller.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$LoginController on _LoginControllerBase, Store {
+  late final _$emailAtom =
+      Atom(name: '_LoginControllerBase.email', context: context);
+
+  @override
+  String get email {
+    _$emailAtom.reportRead();
+    return super.email;
+  }
+
+  @override
+  set email(String value) {
+    _$emailAtom.reportWrite(value, super.email, () {
+      super.email = value;
+    });
+  }
+
+  late final _$passwordAtom =
+      Atom(name: '_LoginControllerBase.password', context: context);
+
+  @override
+  String get password {
+    _$passwordAtom.reportRead();
+    return super.password;
+  }
+
+  @override
+  set password(String value) {
+    _$passwordAtom.reportWrite(value, super.password, () {
+      super.password = value;
+    });
+  }
+
+  late final _$_LoginControllerBaseActionController =
+      ActionController(name: '_LoginControllerBase', context: context);
+
+  @override
+  void setEmail(String value) {
+    final _$actionInfo = _$_LoginControllerBaseActionController.startAction(
+        name: '_LoginControllerBase.setEmail');
+    try {
+      return super.setEmail(value);
+    } finally {
+      _$_LoginControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setPassword(String value) {
+    final _$actionInfo = _$_LoginControllerBaseActionController.startAction(
+        name: '_LoginControllerBase.setPassword');
+    try {
+      return super.setPassword(value);
+    } finally {
+      _$_LoginControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+email: ${email},
+password: ${password}
+    ''';
+  }
+}
