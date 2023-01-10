f1eb9685bc7faa1b1a205319dbb7f4691bcee237
Rodrigo Benício <rodrigobeniciosilva@gmail.com>
2023-01-10 14:20:30 -0300

    update login_controller

--- lib/controllers/login/login_controller.dart
+++ lib/controllers/login/login_controller.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:mobx/mobx.dart';
 part 'login_controller.g.dart';
 
@@ -7,12 +8,27 @@ abstract class _LoginControllerBase with Store {
   @observable
   String email = '';
 
-  @action
-  void setEmail(String value) => email = value;
-
   @observable
   String password = '';
 
+  @observable
+  bool obscurePassword = true;
+
+  @action
+  void setEmail(String value) => email = value;
+
   @action
   void setPassword(String value) => password = value;
+
+  @action
+  void toggleObscurePassword() => obscurePassword = !obscurePassword;
+
+  @computed
+  bool get isEmailValid => EmailValidator.validate(email);
+
+  @computed
+  bool get isPasswordValid => password.length >= 6;
+
+  @computed
+  bool get isFormValid => isEmailValid && isPasswordValid;
 }
--- lib/controllers/login/login_controller.g.dart
+++ lib/controllers/login/login_controller.g.dart
@@ -9,6 +9,28 @@ part of 'login_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$LoginController on _LoginControllerBase, Store {
+  Computed<bool>? _$isEmailValidComputed;
+
+  @override
+  bool get isEmailValid =>
+      (_$isEmailValidComputed ??= Computed<bool>(() => super.isEmailValid,
+              name: '_LoginControllerBase.isEmailValid'))
+          .value;
+  Computed<bool>? _$isPasswordValidComputed;
+
+  @override
+  bool get isPasswordValid =>
+      (_$isPasswordValidComputed ??= Computed<bool>(() => super.isPasswordValid,
+              name: '_LoginControllerBase.isPasswordValid'))
+          .value;
+  Computed<bool>? _$isFormValidComputed;
+
+  @override
+  bool get isFormValid =>
+      (_$isFormValidComputed ??= Computed<bool>(() => super.isFormValid,
+              name: '_LoginControllerBase.isFormValid'))
+          .value;
+
   late final _$emailAtom =
       Atom(name: '_LoginControllerBase.email', context: context);
 
@@ -41,6 +63,22 @@ mixin _$LoginController on _LoginControllerBase, Store {
     });
   }
 
+  late final _$obscurePasswordAtom =
+      Atom(name: '_LoginControllerBase.obscurePassword', context: context);
+
+  @override
+  bool get obscurePassword {
+    _$obscurePasswordAtom.reportRead();
+    return super.obscurePassword;
+  }
+
+  @override
+  set obscurePassword(bool value) {
+    _$obscurePasswordAtom.reportWrite(value, super.obscurePassword, () {
+      super.obscurePassword = value;
+    });
+  }
+
   late final _$_LoginControllerBaseActionController =
       ActionController(name: '_LoginControllerBase', context: context);
 
@@ -67,10 +105,25 @@ mixin _$LoginController on _LoginControllerBase, Store {
   }
 
   @override
+  void toggleObscurePassword() {
+    final _$actionInfo = _$_LoginControllerBaseActionController.startAction(
+        name: '_LoginControllerBase.toggleObscurePassword');
+    try {
+      return super.toggleObscurePassword();
+    } finally {
+      _$_LoginControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
   String toString() {
     return '''
 email: ${email},
-password: ${password}
+password: ${password},
+obscurePassword: ${obscurePassword},
+isEmailValid: ${isEmailValid},
+isPasswordValid: ${isPasswordValid},
+isFormValid: ${isFormValid}
     ''';
   }
 }
