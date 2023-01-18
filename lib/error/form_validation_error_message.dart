79cb0933970a9d01d489af08ae3479e82f3e68ea
Rodrigo Benício <rodrigobeniciosilva@gmail.com>
2023-01-18 14:19:13 -0300

    add: FormValidationErrorMessage class

---
+++ lib/error/form_validation_error_message.dart
@@ -0,0 +1,5 @@
+class FormValidationErrorMessage {
+  final String invalidEmailMessage = 'Endereço de email inválido. Exemplo de email válido: seunome@exemplo.com';
+  final String invalidPasswordMessage = 'A senha deve ter pelo menos 6 caracteres.';
+  final String invalidConfirmPasswordMessage = 'A senha deve ser igual a anterior.';
+}
